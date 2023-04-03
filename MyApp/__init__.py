abb52d529772ea78d4ecc35f5a75d33d04c7f707
MAURICE OGEDEGBE <ogedegbemaurice@gmail.com>
2023-04-03 11:11:18 +0100

    proc and wsgi2

--- MyApp/__init__.py
+++ MyApp/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '41ac0aad7dcefe9f83423ac962f2377a'
-app.config['DATABASE_URL'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
 from MyApp import routes
